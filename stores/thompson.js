62c3af15f3f09f8df7788b11d1fa902419eab793
cigarhaven0-lab <cigarhaven0@gmail.com>
2026-03-24 23:05:42 -0400

    Update thompson.js

--- stores/thompson.js
+++ stores/thompson.js
@@ -4,7 +4,11 @@ async function searchThompson(query) {
       store: "Thompson Cigars",
       name: `${query} (sample result)`,
       price: "$10.99",
-      url: "https://www.thompsoncigar.com/"
+      url: "https://www.thompsoncigar.com/",
+      pack: "Box of 20",
+      inStock: false,
+      lastChecked: new Date().toLocaleString(),
+      sourceType: "sample"
     }
   ];
 }
