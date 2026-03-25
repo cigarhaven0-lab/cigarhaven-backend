95db5316815723108e49258520ce90b4016c97af
cigarhaven0-lab <cigarhaven0@gmail.com>
2026-03-24 23:05:10 -0400

    Update jr.js

--- stores/jr.js
+++ stores/jr.js
@@ -4,7 +4,11 @@ async function searchJR(query) {
       store: "JR Cigars",
       name: `${query} (sample result)`,
       price: "$12.99",
-      url: "https://www.jrcigars.com/"
+      url: "https://www.jrcigars.com/",
+      pack: "Single",
+      inStock: true,
+      lastChecked: new Date().toLocaleString(),
+      sourceType: "sample"
     }
   ];
 }
